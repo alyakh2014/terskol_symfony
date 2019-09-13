f68638d129209333a50fadb4d364c73290bd2ae9
Alexandra <alyakh2014@gmail.com>
2019-09-13 22:18:46 +0300

    add MainController and integrate index page html to template

---
+++ src/Controller/MainController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1
+ * Date: 12.09.2019
+ * Time: 22:56
+ */
+
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response,
+    Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MainController extends Controller
+{
+    public function Index(){
+        return $this->render('main/index.html.twig', array());
+    }
+}
